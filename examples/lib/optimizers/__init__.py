7a2ea666209cb7d6dd0d6cdd3e15c36bd09fb1de
Sohambasu07 <soham.basu07@gmail.com>
2025-01-31 23:01:59 +0100

    Added Random Search with Priors

--- examples/lib/optimizers/__init__.py
+++ examples/lib/optimizers/__init__.py
@@ -2,13 +2,14 @@ from __future__ import annotations
 
 from typing import TYPE_CHECKING
 
-from lib.optimizers.random_search import RandomSearch
+from lib.optimizers.random_search import RandomSearch, RandomSearchWithPriors
 
 if TYPE_CHECKING:
     from hpoglue.optimizer import Optimizer
 
 OPTIMIZERS: dict[str, type[Optimizer]] = {
     RandomSearch.name: RandomSearch,
+    RandomSearchWithPriors.name: RandomSearchWithPriors,
 }
 
 __all__ = [
